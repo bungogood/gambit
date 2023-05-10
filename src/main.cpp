e0cfa95235403ba37b6f2ef93086b131af42eaf4
Jonathan Good <github@bungogood.com>
2023-05-10 20:19:38 +0100

    Testing calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,18 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+#include <Arduino.h>
+#include "motor.hpp"
+#include "hbot.hpp"
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+Motor leftMotor(2, 3, 200);
+Motor rightMotor(4, 5, 200);
+
+HBot hbot(leftMotor, rightMotor, 6);
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Starting...");
+    hbot.calibrate();
+    Serial.println("Calibrated");
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+void loop() {
 }
