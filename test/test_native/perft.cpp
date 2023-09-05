55ed7a0e3045d8c716145b51dd200cf9ae52d925
Jonathan Good <github@bungogood.com>
2023-09-05 19:46:02 +0100

    Updating tests

--- test/test_native/perft.cpp
+++ test/test_native/perft.cpp
@@ -15,7 +15,7 @@ Depth	Nodes
 5	4,865,609
 */
 
-int search_perf(int side, int en_passant, Chess* chess, int depth) {
+int perft(int side, int en_passant, Chess* chess, int depth) {
     if (depth == 0) return 1;
 
     Move_List possible;
@@ -25,47 +25,47 @@ int search_perf(int side, int en_passant, Chess* chess, int depth) {
 
     for (int i = 0; i < possible.length; i++) {     // loop over move list
         chess->make_move(possible.moves[i], side);  // make move
-        count += search_perf(24 - side, possible.moves[i].skip_square, chess,
-                             depth - 1);
+        count +=
+            perft(24 - side, possible.moves[i].skip_square, chess, depth - 1);
         chess->unmake_move(possible.moves[i], side);  // take back
     }
 
     return count;
 }
 
-void test_depth_1() {
+void testDepth1() {
     Chess chess;
-    TEST_ASSERT_EQUAL(20, search_perf(8, 128, &chess, 1));
+    TEST_ASSERT_EQUAL(20, perft(8, 128, &chess, 1));
 }
 
-void test_depth_2() {
+void testDepth2() {
     Chess chess;
-    TEST_ASSERT_EQUAL(400, search_perf(8, 128, &chess, 2));
+    TEST_ASSERT_EQUAL(400, perft(8, 128, &chess, 2));
 }
 
-void test_depth_3() {
+void testDepth3() {
     Chess chess;
-    TEST_ASSERT_EQUAL(8902, search_perf(8, 128, &chess, 3));
+    TEST_ASSERT_EQUAL(8902, perft(8, 128, &chess, 3));
 }
 
-void test_depth_4() {
+void testDepth4() {
     Chess chess;
     TEST_IGNORE_MESSAGE("Currently failing");
-    TEST_ASSERT_EQUAL(197281, search_perf(8, 128, &chess, 4));
+    TEST_ASSERT_EQUAL(197281, perft(8, 128, &chess, 4));
 }
 
-void test_depth_5() {
+void testDepth5() {
     Chess chess;
     TEST_IGNORE_MESSAGE("Currently failing");
-    TEST_ASSERT_EQUAL(4865609, search_perf(8, 128, &chess, 5));
+    TEST_ASSERT_EQUAL(4865609, perft(8, 128, &chess, 5));
 }
 
 int main(int argc, char** argv) {
     UNITY_BEGIN();
-    RUN_TEST(test_depth_1);
-    RUN_TEST(test_depth_2);
-    RUN_TEST(test_depth_3);
-    RUN_TEST(test_depth_4);
-    RUN_TEST(test_depth_5);
+    RUN_TEST(testDepth1);
+    RUN_TEST(testDepth2);
+    RUN_TEST(testDepth3);
+    RUN_TEST(testDepth4);
+    RUN_TEST(testDepth5);
     UNITY_END();
 }
