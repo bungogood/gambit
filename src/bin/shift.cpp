fdb5a6fcd6c587382853449a0806d7c63a66cced
mark-a-austin <s2038222@ed.ac.uk>
2024-09-20 14:22:20 +0100

    fix: updated matrix.cpp and shift.cpp to comply with the lint

--- src/bin/shift.cpp
+++ src/bin/shift.cpp
@@ -1,25 +1,25 @@
 #include <Arduino.h>
-#include "indicator.hpp"
+
 #include "bluetooth.hpp"
+#include "indicator.hpp"
 
 Indicator indicator(LED_RED, LED_GREEN, LED_BLUE);
 
-
 //   a  b  c  d  e  f  g   h
 // 8 .  .  .  .  .  .  .   .  8 D8
-                       
+
 // 7 .  .  .  .  .  .  .   .  7 D7
-                       
+
 // 6 .  .  .  .  .  .  .   .  6 D6
-                       
+
 // 5 .  .  .  .  .  .  .   .  5 D5
-                       
+
 // 4 .  .  .  .  .  .  .   .  4 D4
-                       
+
 // 3 .  .  .  .  .  .  .   .  3 D3
-                       
+
 // 2 .  .  .  .  .  .  .   .  2 D2
-                       
+
 // 1 .  .  .  .  .  .  .   .  1 D1 (TX1)
 //  15  1  2  3  4  5  6   7 (595)
 
@@ -30,29 +30,24 @@ const bool SHIFT_IN = true;
 int boardState[8][8];
 BluetoothManager ble;
 
-
-//74HC595 pins
+// 74HC595 pins
 const int shiftOutDataPin = A0;
 const int shiftOutClockPin = A1;
 const int shiftOutLatchPin = A2;
 
+// 74HC165 pins
+const int shiftInClockEnablePin = D3;
+const int shiftInDataPin = D4;
+const int shiftInClockPin = D2;
+const int shiftInLoadPin = D1;
 
-
-//74HC165 pins
-const int shiftInClockEnablePin = D9;
-const int shiftInDataPin = D10;
-const int shiftInClockPin = D11;
-const int shiftInLoadPin = D12;
-
-
-//shift in consts
+// shift in consts
 const int PULSE_WIDTH_USEC = 5;
-
+const int LOAD_PULSE_WIDTH = 30;
+const int CLOCK_PULSE_DELAY = 5;
 
 byte shiftOutData;
 byte inputData;
-
-
 
 void setup() {
     Serial.begin(9600);
@@ -78,44 +73,64 @@ void setup() {
         pinMode(shiftInClockPin, OUTPUT);
         pinMode(shiftInDataPin, INPUT);
         pinMode(shiftInClockEnablePin, OUTPUT);
-    } else{
+        digitalWrite(shiftInClockEnablePin, LOW);
+    } else {
         for (int i = 0; i < 8; i++) {
             pinMode(ONE_EIGHT_PINS[i], INPUT_PULLDOWN);
         }
     }
-
 }
 
-byte shiftIn(int dataPin, int clockPin, int bitOrder){
+byte shiftIn(int dataPin, int clockPin, int bitOrder) {
     byte value = 0;
-    long bitValue;
-    for (int i =0; i < 8; i++){
-        digitalWrite(clockPin, HIGH);
-        delayMicroseconds(10);
+    byte bitValue;
+    for (int i = 0; i < 8; i++) {
         bitValue = digitalRead(dataPin);
+        Serial.print(bitValue);
 
-        if (bitOrder == LSBFIRST){
+        if (bitOrder == LSBFIRST) {
             value |= (bitValue << i);
         } else {
             value |= (bitValue << (7 - i));
         }
+        digitalWrite(clockPin, HIGH);
+        delayMicroseconds(CLOCK_PULSE_DELAY);
         digitalWrite(clockPin, LOW);
-        delayMicroseconds(10);
+        delayMicroseconds(CLOCK_PULSE_DELAY);
     }
     return value;
 }
 
-void shiftOut(int dataPin, int clockPin, byte dataOut){
-    int i=0;
+void printInputData(byte data, int pinSetHigh) {
+    // For each of the data pins print the value
+    // Print the labels of each pins
+    Serial.println("--------------------");
+    Serial.println("Input Data with D" + String(pinSetHigh) + " set high");
+    for (int i = 0; i < 8; i++) {
+        Serial.print("D");
+        Serial.print(i);
+        Serial.print(" ");
+    }
+    Serial.println();
+    for (int i = 0; i < 8; i++) {
+        Serial.print((data & (1 << i)) >> i);
+        Serial.print("  ");
+    }
+    Serial.println();
+    Serial.println();
+    Serial.println("--------------------");
+}
+
+void shiftOut(int dataPin, int clockPin, byte dataOut) {
+    int i = 0;
     int pinState;
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
-    for (i = 7; i >= 0; i--)  {
+    for (i = 7; i >= 0; i--) {
         digitalWrite(clockPin, LOW);
-        if ( dataOut & (1 << i) ) {
+        if (dataOut & (1 << i)) {
             pinState = HIGH;
-        }
-        else {
+        } else {
             pinState = LOW;
         }
         digitalWrite(dataPin, pinState);
@@ -125,30 +140,29 @@ void shiftOut(int dataPin, int clockPin, byte dataOut){
     digitalWrite(clockPin, LOW);
 }
 
-void loop(){
+void loop() {
     for (int col = 0; col < 8; col++) {
         shiftOutData = A_H_ACTIVATION_NUMS[col];
         digitalWrite(shiftOutLatchPin, LOW);
         shiftOut(shiftOutDataPin, shiftOutClockPin, shiftOutData);
         digitalWrite(shiftOutLatchPin, HIGH);
-        delayMicroseconds(5);
+        delayMicroseconds(30);
 
-
-        if (SHIFT_IN){
+        if (SHIFT_IN) {
             // load shiftin register
             digitalWrite(shiftInLoadPin, LOW);
-            delayMicroseconds(50);
+            delayMicroseconds(LOAD_PULSE_WIDTH);
             digitalWrite(shiftInLoadPin, HIGH);
-
+            delayMicroseconds(LOAD_PULSE_WIDTH);
 
             inputData = shiftIn(shiftInDataPin, shiftInClockPin, MSBFIRST);
-
+            // printInputData(inputData, col);
+            // delay(500);
 
             for (int row = 0; row < 8; row++) {
                 boardState[row][col] = (inputData >> row) & 1;
             }
-        }
-        else{
+        } else {
             for (int row = 0; row < 8; row++) {
                 boardState[row][col] = digitalRead(ONE_EIGHT_PINS[row]);
             }
@@ -182,7 +196,8 @@ void loop(){
     // uint64_t boardStateU64 = 0;
     // for (int row = 0; row < 8; row++) {
     //     for (int col = 0; col < 8; col++) {
-    //         boardStateU64 |= (uint64_t)boardState[row][col] << (row * 8 + col);
+    //         boardStateU64 |= (uint64_t)boardState[row][col] << (row * 8 +
+    //         col);
     //     }
     // }
     // ble.setReedSwitchValue(boardStateU64);
